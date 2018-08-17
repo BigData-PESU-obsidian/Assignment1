a6e76d5506d20f945fa064ccf6014bd47754ec70
Yashas ND <yashasbharadwaj111@gmail.com>
2018-08-17 19:33:48 +0530

    Initial commit, yaml -> csv done

--- python/analyse.py
+++ python/analyse.py
@@ -1,19 +1,6 @@
 import yaml
 import os
 
-def filespath():
-    return(os.listdir(os.getcwd()+"/../data"))
-
-def batsmanrate():
-    matches = filespath()
-    for matchfile in matches:
-        fp = open("./../data/"+matchfile, "r")
-        matchData = yaml.load(fp)
-        fp.close()
-        
-def bowler():
-    pass
-
 if(__name__ == "__main__"):
     batsmanrate()
     
